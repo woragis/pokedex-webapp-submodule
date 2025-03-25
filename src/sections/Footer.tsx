69da4c1311337b33aacaec6e3f94fd16ca97d45a
woragis <masteringthecode.woragis+github@gmail.com>
2025-03-24 22:53:07 -0300

    Add footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { MdOutlinePowerSettingsNew } from 'react-icons/md'
 
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer>
+      <div className='block'></div>
+      <div className='data'></div>
+      <div className='block'>
+        <MdOutlinePowerSettingsNew />
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
