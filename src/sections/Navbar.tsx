4b00e4103fcf71b756fc97c5c2cbf570735bc895
woragis <masteringthecode.woragis+github@gmail.com>
2025-03-24 22:50:07 -0300

    Add navbar section

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
+import pokeballIcon from '@/assets/pokeball-icon.png'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <nav>
+      <div className='block'>
+        <Image
+          src={pokeballIcon}
+          alt='Pokeball icon'
+        />
+      </div>
+      <div className='data'></div>
+      <div className='block'>
+        <GiHamburgerMenu />
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
