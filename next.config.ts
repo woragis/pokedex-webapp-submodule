96942e30b9875067ef1b31660bca5f3dce2c2c9e
woragis <masteringthecode.woragis+github@gmail.com>
2025-03-28 01:28:47 -0300

    Update image component to next/image Image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pokeapi.co',
+      },
+    ],
+  },
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default nextConfig
--- src/components/PokemonCardGrid.tsx
+++ src/components/PokemonCardGrid.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { PokemonData } from '@/store/pokemon'
+import Image from 'next/image'
 import Link from 'next/link'
 
 interface PokemonCardGridProps {
-  pokemons?: PokemonData[]
+  pokemons: PokemonData[]
 }
+
 function PokemonCardGrid({ pokemons }: PokemonCardGridProps) {
-  console.log(pokemons)
   return (
     <div className='pokemon-card-grid-container'>
       {pokemons &&
@@ -19,7 +20,7 @@ function PokemonCardGrid({ pokemons }: PokemonCardGridProps) {
               key={`listed-pokemon-${pokemon.id}`}
             >
               <Link href={`/pokemon/${pokemon.id}`}>
-                <img
+                <Image
                   width={100}
                   height={100}
                   src={pokemon.sprites.front_default}
